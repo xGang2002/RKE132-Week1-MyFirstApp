3a452b35ade3b41228ddd6946d4683cfadf4bca2
Sanya <sassik6969@gmail.com>
2023-02-01 17:37:52 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// Ya loh
+
+//rakendus küsib kasutajalt sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:"); // output
+//string on sõne 
+string userName = Console.ReadLine(); //input
+
+Console.WriteLine("Здарова" +", " + userName + "!"); //output
+//string interpolation
+
+Console.WriteLine($"Здарова, {userName} !");//output
